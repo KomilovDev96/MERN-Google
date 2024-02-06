7d4d8ce8bc1540edf2daf8a32f601e8e5443af64
Solihov <islomjonsolihov@gmail.com>
2024-02-06 12:04:49 +0500

    dlt

--- scoolproject/src/store/index.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import counterSlice from './slice/ikrement'
-export const store = configureStore({
-    devTools: true,
-    reducer: {
-        counter: counterSlice,
-    },
-})
