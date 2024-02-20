2595588b0935efd0d1cc44311fd6f9f8608c4e3d
Azizbek1 <azizochilov569@gmail.com>
2024-02-20 11:21:15 +0500

    popu

--- CloudCLient/src/components/popup/index.jsx
+++ CloudCLient/src/components/popup/index.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Input from '../Input'
+import { useSelector } from 'react-redux'
 
 export default function Popup() {
+    const popupDisplay = useSelector(state => state.file.popupDisplay)
     return (
-        <div className='popup'>
+        <div className='popup' style={{ display: popupDisplay }}>
             <div className='popup__content'>
                 <div className='popup__header'>
                     <div className='popup_title'>Создать папку</div>
--- CloudCLient/src/pages/DiscPage/index.jsx
+++ CloudCLient/src/pages/DiscPage/index.jsx
@@ -1,15 +1,20 @@
 import Popup from "@/components/popup";
 import FileList from "./components/FileList";
 import "./style.scss"
+import { useDispatch } from "react-redux";
+import { SetPopapDisplay } from "@/store/Features/File.slice";
 
 
 const DiscPage = () => {
-
+    const dispatch = useDispatch()
+    const showPopupHundler = () => {
+        dispatch(SetPopapDisplay('flex'))
+    }
     return (
         <div className="disk">
             <div className="disk__btns">
                 <button className="disk__back">Назад</button>
-                <button className="disk__create">Создать папку</button>
+                <button className="disk__create" onClick={() => showPopupHundler()}>Создать папку</button>
             </div>
             <FileList />
             <Popup />
