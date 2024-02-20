9e5cfa1c1a8748c88ff9913ed755dc571c6d81ae
Azizbek1 <azizochilov569@gmail.com>
2024-02-20 11:35:23 +0500

    dispatch

--- CloudCLient/src/components/popup/index.jsx
+++ CloudCLient/src/components/popup/index.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
 import Input from '../Input'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { SetPopapDisplay } from '@/store/Features/File.slice'
 
 export default function Popup() {
     const popupDisplay = useSelector(state => state.file.popupDisplay)
+    const dispatch = useDispatch()
+    const hidePopupHundler = () => {
+        dispatch(SetPopapDisplay('none'))
+    }
     return (
         <div className='popup' style={{ display: popupDisplay }}>
             <div className='popup__content'>
                 <div className='popup__header'>
                     <div className='popup_title'>Создать папку</div>
-                    <button>X</button>
+                    <button onClick={() => hidePopupHundler()}>X</button>
                 </div>
                 <Input />
                 <button className='popup__create'> Создать </button>
